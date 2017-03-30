eab2e61aeb082bacf3c55d319f37094ffa887402
Justin Calleja <calleja.justin@gmail.com>
2017-03-31 00:58:30 +0200

    saving state

--- repl.js
+++ repl.js
@@ -1,20 +1,30 @@
+const mapAll = require('@justinc/map-all')
 const graphlib = require('graphlib')
 const Task = require('folktale/data/task')
 const repl = require('repl')
-const gNode = require('./lib/gNode')
+const gNode = require('./lib/gnode')
 const utils = require('./lib/utils')
+const Result = require('folktale/data/result')
+const zipWith = require('lodash.zipwith')
+const validateUnzippedGNodes = require('./lib/validate-unzipped-gnodes')
+const compose = require('folktale/core/lambda/compose').all
+const { dependencyEdges, dependentEdges } = require('./lib/edges')
+const { addNodes, addEdges } = require('./lib/graph')
 // const { defaultUnzippers } = require('./lib/unzippers')
+
+const unzipGNodeAsValidation = gNode.unzipAsValidation()
 
 const r = repl.start('> ')
 
 r.context.Task = Task
+r.context.Result = Result
 
 r.context.dagIt = {
   gNode,
   utils
 }
 
-r.context.gNodes = [
+const gNodes = r.context.gNodes = [
   { id: 'N0', dependencies: [ 'N4' ] },
   { id: 'N1', dependencies: [ 'N4' ], onVisit: (env, id) => `id:${id}` },
   { id: 'N2' },
@@ -23,6 +33,24 @@ r.context.gNodes = [
   { id: 'N5', dependencies: [ 'N0' ] }
 ]
 
-const res = r.context.res = r.context.gNodes.map(gNode.unzipAsValidation())
+const unzippedGNodesAsValidation = r.context.unzippedGNodesAsValidation = utils.combineValidations(gNodes.map(unzipGNodeAsValidation))
+const unzippedGNodesAsResult = r.context.unzippedGNodesAsResult = Result.fromValidation(unzippedGNodesAsValidation)
+// const validatedUnzippedGNodesAsResult = r.context.validatedUnzippedGNodesAsResult =
 
-r.context.unzippedGNodesAsValidation = utils.combineValidations(res)
+const graphAsResult = r.context.graphAsResult = compose(
+  // ⬆ :: Graph
+  utils.map(([ids, allDependencies, allDependents]) => {
+    const edges = [ ...dependencyEdges(ids, allDependencies), ...dependentEdges(ids, allDependents) ]
+    const graph = new graphlib.Graph()
+    addNodes(graph)(gNodes)(ids)
+    addEdges(graph)(edges)
+    return graph
+  }),
+  // ⬆ :: Tuple< ids::Array<String>, allDependencies::Array<Array<String>>, allDependents::Array<Array<String>> >
+  utils.map(mapAll([
+    unzippedGNode => unzippedGNode[0],
+    unzippedGNode => unzippedGNode[1],
+    unzippedGNode => unzippedGNode[2]
+  ])),
+  utils.chain(validateUnzippedGNodes)
+)(unzippedGNodesAsResult)
