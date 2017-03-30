38db14ba6899360d5d35dc6aca5eef80a36f1ea8
Justin Calleja <calleja.justin@gmail.com>
2017-03-30 10:47:19 +0200

    tmp

--- test/repl.js
+++ test/repl.js
@@ -1,19 +1,28 @@
 const repl = require('repl')
 const Task = require('folktale/data/task');
-const dagNodesToGParts = require('../lib/dagnodes-to-gparts')
-const dagNodesToGPartsAsValidations = require('../lib/dagnodes-to-gparts/as-validations')
+const gNodesToGParts = require('../lib/gnodes-to-gparts')
+const gNodesToGraph = require('../lib/gnodes-to-graph')
+const gNodesToGPartsAsValidations = require('../lib/gnodes-to-gparts/as-validations')
 const gPartsToEdges = require('../lib/gparts-to-edges')
 const { dependencyEdges, dependentEdges } = require('../lib/gparts-to-edges/edges')
 const mappers = require('../lib/mappers')
+const defaults = require('../lib/mappers/defaults')
 const validateGParts = require('../lib/validate-gparts')
+const utils = require('../lib/utils')
 
 const r = repl.start('> ')
 
 r.context.Task = Task
-r.context.dagNodesToGParts = dagNodesToGParts
-r.context.dagNodesToGPartsAsValidations = dagNodesToGPartsAsValidation
-r.context.gPartsToEdges = gPartsToEdges
-r.context.dependencyEdges = dependencyEdges
-r.context.dependentEdges = dependentEdges
-r.context.mappers = mappers
-r.context.validateGParts = validateGParts
+
+r.context.dagIt = {
+  gNodesToGParts,
+  gNodesToGraph,
+  gNodesToGPartsAsValidations,
+  gPartsToEdges,
+  dependencyEdges,
+  dependentEdges,
+  utils,
+  mappers,
+  defaults,
+  validateGParts
+}
